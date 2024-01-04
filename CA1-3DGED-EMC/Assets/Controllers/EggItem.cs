5bc50f4cb7afeb5c59f0c6a3ab8dc91b61bc6bd9
EmilyMcCormack <D00230520@student.dkit.ie>
2024-01-04 14:28:02 +0000

    :hammer: Updated scripts for pickup items

--- CA1-3DGED-EMC/Assets/Controllers/EggItem.cs
+++ CA1-3DGED-EMC/Assets/Controllers/EggItem.cs
@@ -4,23 +4,13 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
-public class CakeItem : MonoBehaviour
+public class EggItem : MonoBehaviour
 {
-    //public GameObject Egg;
-    bool isEggDestroyed = false;
-    //public GameObject blood;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Egg"))
         {
             GameVariables.eggCount += 1;
-            Destroy(gameObject);
-            isEggDestroyed = true;
-
-        }
-        else if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("blood") && isEggDestroyed == true)
-        {
-            GameVariables.bloodCount += 1;
             Destroy(gameObject);
         }
     }
