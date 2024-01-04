255ad2c22edfd8cd82ef65e36f54d43bfcb1737d
EmilyMcCormack <D00230520@student.dkit.ie>
2024-01-04 14:13:59 +0000

    :construction: Pickups working, changed to collection

---
+++ CA1-3DGED-EMC/Assets/Controllers/CakeItem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class CakeItem : MonoBehaviour
+{
+    //public GameObject Egg;
+    bool isEggDestroyed = false;
+    //public GameObject blood;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("Egg"))
+        {
+            GameVariables.eggCount += 1;
+            Destroy(gameObject);
+            isEggDestroyed = true;
+
+        }
+        else if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("blood") && isEggDestroyed == true)
+        {
+            GameVariables.bloodCount += 1;
+            Destroy(gameObject);
+        }
+    }
+        
+            
+               
+
+}
---
+++ CA1-3DGED-EMC/Assets/Controllers/GameItems.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameVariables : MonoBehaviour
+{
+    public static int eggCount;
+    public static int bloodCount;
+}
